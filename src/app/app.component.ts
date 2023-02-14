b26390a9e960919e89409527e69e15e9c04d5f41
Leighton Meredith <luluket2000@gmail.com>
2023-02-14 13:27:51 -0500

    select specific side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,9 +49,11 @@ export class AppComponent implements OnInit {
   rightPlayEnd: any;
 
   playLeft: boolean;
+  pickSide: boolean;
 
   constructor() {
-    this.playLeft = false
+    this.playLeft = false;
+    this.pickSide = false;
   }
 
   ngOnInit() {
@@ -61,19 +63,19 @@ export class AppComponent implements OnInit {
   getDominoes() {
     for (let a = 0; a < 7; a++) {
       const tile1 = this.pullTiles();
-      this.hand1.push({key: tile1.tile, img: `../assets/img/${tile1.tile}.png`})
+      this.hand1.push({key: tile1.tile, img: `../assets/img/${tile1.tile}.png`, reverse: false, turn: tile1.tile === '6-6' })
       tile1.isSelected = true
 
       const tile2 = this.pullTiles();
-      this.hand2.push({key: tile2.tile, img: `../assets/img/${tile2.tile}.png`})
+      this.hand2.push({key: tile2.tile, img: `../assets/img/${tile2.tile}.png`, reverse: false, turn: tile1.tile === '6-6' })
       tile2.isSelected = true;
 
       const tile3 = this.pullTiles();
-      this.hand3.push({key: tile3.tile, img: `../assets/img/${tile3.tile}.png`})
+      this.hand3.push({key: tile3.tile, img: `../assets/img/${tile3.tile}.png`, reverse: false, turn: tile1.tile === '6-6' })
       tile3.isSelected = true
 
       const tile4 = this.pullTiles();
-      this.hand4.push({key: tile4.tile, img: `../assets/img/${tile4.tile}.png`})
+      this.hand4.push({key: tile4.tile, img: `../assets/img/${tile4.tile}.png`, reverse: false, turn: tile1.tile === '6-6' })
       tile4.isSelected = true;
     }
 
@@ -87,25 +89,51 @@ export class AppComponent implements OnInit {
       return filteredTiles[Math.floor(Math.random()*filteredTiles.length)];
   }
 
+  selectedHand: any[] = [];
+  selectedIndex: any = {};
+
   playTile(tileInfo: any, index: any, hand: any[]) {
+    const TILE_SIDES = tileInfo.key.split('-');
     if(!this.isValidTile(tileInfo)){
       return
     }
+
+    if( this.isNonDouble(tileInfo) &&
+        TILE_SIDES.includes(this.leftPlayEnd) &&
+        TILE_SIDES.includes(this.rightPlayEnd) &&
+        this.leftPlayEnd !== this.rightPlayEnd) {
+
+          this.pickSide = true;
+
+          this.selectedHand = hand;
+          this.selectedIndex = index;
+          return;
+    }
+
+    this.pickSide = false
+    if(TILE_SIDES.includes(this.leftPlayEnd) ) {
+      this.playLeft = true;
+    } else {
+      this.playLeft = false;
+    }
+
 
     this.displayOnTable(hand.at(index))
     hand.splice(index, 1)
   }
 
   displayOnTable(tileInfo: any) {
+    const TILE_SIDES = tileInfo.key.split('-');
     if(this.playedTiles.length === 0) {
-      this.leftPlayEnd = tileInfo.key.split('-')[0];
-      this.rightPlayEnd = tileInfo.key.split('-')[1];
+      this.leftPlayEnd = TILE_SIDES[0];
+      this.rightPlayEnd = TILE_SIDES[1];
     }
-
-
     if(this.playLeft) {
+      tileInfo.reverse = this.isNonDouble(tileInfo) && TILE_SIDES[1] === this.leftPlayEnd
+      this.leftPlayEnd = this.updatePlayEnd(tileInfo, this.leftPlayEnd)
       this.playedTiles.unshift(tileInfo)
     } else {
+      tileInfo.reverse = this.isNonDouble(tileInfo) && TILE_SIDES[0] === this.rightPlayEnd
       this.rightPlayEnd = this.updatePlayEnd(tileInfo, this.rightPlayEnd)
       this.playedTiles.push(tileInfo)
     }
@@ -137,6 +165,16 @@ export class AppComponent implements OnInit {
     const TILE_SIDES = tileInfo.key.split('-');
 
     return TILE_SIDES.find((x: any) => !x.includes(end)) ?? end;
+  }
+
+  placeTile(choseLeft: boolean) {
+    console.log(choseLeft)
+
+    this.playLeft = choseLeft;
+    this.pickSide = false
+
+    this.displayOnTable(this.selectedHand.at(this.selectedIndex))
+    this.selectedHand.splice(this.selectedIndex, 1)
   }
 }
 
